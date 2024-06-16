a5f1388a1b398fc451ffba3df13e0312587b3a41
YegorChevardin <egor03052004@gmail.com>
2024-06-16 18:17:09 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { createReactEditorJS } from "react-editor-js";
+import { EDITOR_JS_TOOLS } from "./tool";
+import React from "react";
 
 function App() {
+  const ReactEditorJS = createReactEditorJS();
+  const editorCore = React.useRef(null);
+  const [data, setData] = useState({});
+
+  const handleInitialize = React.useCallback((instance: any) => {
+    editorCore.current = instance;
+  }, []);
+
+  const handleSave = React.useCallback(async () => {
+    const savedData = await editorCore.current.save();
+    setData(savedData);
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container m-5">
+      <div className="d-flex flex-column">
+        <div className="d-flex align-items-center justify-content-start">
+          <button className="btn btn-sm btn-dark" onClick={handleSave}>
+            Save
+          </button>
+        </div>
+        <div className="border mt-5 p-4 rounded">
+          <ReactEditorJS
+            tools={EDITOR_JS_TOOLS}
+            onInitialize={handleInitialize}
+          />
+        </div>
+      </div>
     </div>
   );
 }
